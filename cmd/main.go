b9db1797b2d8772b2583ea9ea595188d61c6c83f
Vivek Singh <viveksingh9301@gmail.com>
2026-01-02 15:46:57 +0530

    Implement server startup with configurable port in main function

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/joho/godotenv"
 )
@@ -13,4 +15,11 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9090"
+	}
+	log.Println("Server started at port:", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
