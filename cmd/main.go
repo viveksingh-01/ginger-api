6851746fee528e080c6bde04a474b8bdbb58507d
Vivek Singh <viveksingh9301@gmail.com>
2026-01-02 15:44:27 +0530

    Add godotenv package and load environment variables in main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Welcome to Ginger API.")
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/viveksingh-01/ginger-api
 
 go 1.25.5
+
+require github.com/joho/godotenv v1.5.1 // indirect
