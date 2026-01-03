dec26a98f286bfb1d2cfd24688fa943fb2f9551c
Vivek Singh <viveksingh9301@gmail.com>
2026-01-03 20:37:09 +0530

    Add HTTP request creation in HandleRestaurants function

--- handlers/restaurants.go
+++ handlers/restaurants.go
@@ -13,4 +13,12 @@ func HandleRestaurants(w http.ResponseWriter, r *http.Request) {
 	if query.Get("lat") != "" && query.Get("lng") != "" {
 		apiURL += fmt.Sprintf("?lat=%s&lng=%s", query.Get("lat"), query.Get("lng"))
 	}
+
+	// Create HTTP request
+	req, err := http.NewRequest(http.MethodGet, apiURL, nil)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error creating request: %v", err), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(req.URL)
 }
